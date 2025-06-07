7f79950a8d4d3d64d919166855197cf975673519
Alejandra Camelo Cruz <camelo.cruz@leibniz-zas.de>
2025-06-08 00:27:49 +0200

    cleaning

--- frontend/src/api.js
+++
@@ -1,5 +0,0 @@
-export async function fetchJobs() {
-  const response = await fetch("http://localhost:8000/jobs/list");
-  if (!response.ok) throw new Error("Failed to fetch jobs");
-  return await response.json();
-}
